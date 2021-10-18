d30dad07519f0e549fd640708531b4e671df9c55
Jagadeesh Lakshmanan <jagadeeshprithviraj@gmail.com>
2021-10-19 00:21:05 +0530

    fix: seq num update logic

--- apiRouter.js
+++ apiRouter.js
@@ -3,13 +3,28 @@ const apiRouter = express.Router();
 const {UrlShorts} = require('./model')
 
 apiRouter.post('/shorturl', (req, res) => {
-  console.log(req.body);
   const url_input = req.body.url;
-  UrlShorts.create({longUrl: url_input}, (err, doc) => {
-      if (err) return res.json({status: 'failed', error: err});
-      res.json({status: 'success', shortUrl: doc.shortUrl});
-  }); 
-})
+  UrlShorts.find({longUrl: url_input}, (err, doc) => {
+    if (err) return res.status(400).json({status: 'failed to find', error: err});
+    if (doc.length > 0) {
+      console.log('url present already', doc[0].shortUrl);
+      return res.status(204).send();
+    };
+    UrlShorts
+    .find({})
+    .select('shortUrl')
+    .sort({shortUrl: -1})
+    .limit(1)
+    .exec((err, doc) => {
+      if (err) return res.json({status: 'failed to get max', error: err});
+      const shortUrl = (doc.length === 0) ? 0 : doc[0].shortUrl + 1;
+      UrlShorts.create({longUrl: url_input, shortUrl}, (err, doc) => {
+        if (err) return res.json({status: 'failed to create', error: err});
+        res.json({status: 'success', shortUrl: doc.shortUrl});
+      });    
+    })
+  })
+}); 
 
 apiRouter.get('/shorturl/:num', (req, res) => {
   UrlShorts.findOne({shortUrl: req.params.num}, (err, doc) => {
@@ -17,5 +32,12 @@ apiRouter.get('/shorturl/:num', (req, res) => {
     res.redirect(doc.longUrl)
   });
 });
+
+apiRouter.get('/shorturls', (req, res) => {
+  UrlShorts.find({}, (err, docs)=> {
+    if (err) return res.json({status: 'failed retrieval', error: err});
+    res.json({status: 'success', docs})
+  })
+})
 
 exports.router = apiRouter;
