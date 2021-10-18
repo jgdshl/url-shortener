39f39004a8d4428930ba2cd61f878fe7f9bd9322
Jagadeesh Lakshmanan <jagadeeshprithviraj@gmail.com>
2021-10-18 23:45:59 +0530

    included apiRouter for api/

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -22,8 +23,7 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-
-app.use('/api', apiRouter);
+app.use('/api', apiRouter.router);
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
