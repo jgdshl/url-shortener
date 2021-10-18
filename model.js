56b3d72314733bd0ee29ecc93fd53ae58d1e69be
Jagadeesh Lakshmanan <jagadeeshprithviraj@gmail.com>
2021-10-19 00:21:50 +0530

    fix: removed pre-save logic from model

--- model.js
+++ model.js
@@ -23,21 +23,6 @@ const urlShortsSchema = new mongoose.Schema({
   }
 });
 
-urlShortsSchema.pre('save', function(next) {
-    if (!this.isNew) return next();
-    UrlShorts
-      .find({})
-      .select('shortUrl')
-      .sort({shortUrl: -1})
-      .limit(1)
-      .exec((err, doc) => {
-          if (err) return next(err);
-          if (doc.length === 0) return this.shortUrl = 0;
-          this.shortUrl = doc[0].shortUrl + 1;
-      })
-    next();
-});
-
 const UrlShorts = mongoose.model('UrlShorts', urlShortsSchema);
 
 exports.UrlShorts = UrlShorts;
